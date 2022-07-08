f74941fc2dd960f7ca32b36727b299d67649e713
5Mall5Mall <chenhanglong@hollysys.com>
2022-07-08 09:35:28 +0800

    hot-fix second commit

--- src/Test.java
+++ src/Test.java
@@ -6,6 +6,8 @@ public class Test {
         System.out.println("1111");
         System.out.println("第三次修改");
         System.out.println("hot-fix commit1");
+        System.out.println("hot-fix commit2");
+        System.out.println("hot-fix commit3");
 
     }
 
