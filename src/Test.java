fbdf8f735f708604e5fb0f1e204506f385927f2d
5Mall5Mall <chenhanglong@hollysys.com>
2022-07-08 09:16:34 +0800

    master first commit

--- src/Test.java
+++ src/Test.java
@@ -5,7 +5,9 @@ public class Test {
         System.out.println("git test");
         System.out.println("1111");
         System.out.println("第三次修改");
-        System.out.println("hot-fix commit1");
+        System.out.println("master commit1");
+        System.out.println("master commit2");
+        System.out.println("master commit3");
 
     }
 
